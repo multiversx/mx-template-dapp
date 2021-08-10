b4f54879e572d3283441405616203abfcec9b8cb
Tudor Morar <tudor.morar@elrond.com>
2021-08-10 09:33:53 +0300

    Add gasLimit and gasPerDataByte to config

--- src/config.testnet.tsx
+++ src/config.testnet.tsx
@@ -1,24 +1,25 @@
-import * as Dapp from "@elrondnetwork/dapp";
+import * as Dapp from '@elrondnetwork/dapp';
 
-export const dAppName = "Dapp";
+export const dAppName = 'Dapp';
 export const decimals = 2;
 export const denomination = 18;
 export const gasPrice = 1000000000;
+export const gasLimit = 50000;
+export const gasPerDataByte = 1500;
 export const version = 1;
 
-export const walletConnectBridge = "https://bridge.walletconnect.org";
+export const walletConnectBridge = 'https://bridge.walletconnect.org';
 export const walletConnectDeepLink =
-  "https://maiar.page.link/?apn=com.elrond.maiar.wallet&isi=1519405832&ibi=com.elrond.maiar.wallet.dev&link=https://maiar.com/";
+  'https://maiar.page.link/?apn=com.elrond.maiar.wallet&isi=1519405832&ibi=com.elrond.maiar.wallet.dev&link=https://maiar.com/';
 
-export const contractAddress =
-  "erd1qqqqqqqqqqqqqpgqp699jngundfqw07d8jzkepucvpzush6k3wvqyc44rx";
+export const contractAddress = 'erd1qqqqqqqqqqqqqpgqp699jngundfqw07d8jzkepucvpzush6k3wvqyc44rx';
 
 export const network: Dapp.NetworkType = {
-  id: "testnet",
-  name: "Testnet",
-  egldLabel: "xEGLD",
-  walletAddress: "https://testnet-wallet.elrond.com",
-  apiAddress: "https://testnet-api.elrond.com",
-  gatewayAddress: "https://testnet-gateway.elrond.com",
-  explorerAddress: "http://testnet-explorer.elrond.com/",
+  id: 'testnet',
+  name: 'Testnet',
+  egldLabel: 'xEGLD',
+  walletAddress: 'https://testnet-wallet.elrond.com',
+  apiAddress: 'https://testnet-api.elrond.com',
+  gatewayAddress: 'https://testnet-gateway.elrond.com',
+  explorerAddress: 'http://testnet-explorer.elrond.com/',
 };
