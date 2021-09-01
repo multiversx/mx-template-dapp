4aca70d60e792f2809b1b19b50095973bbbaa2ff
Tudor Morar <tudor.morar@elrond.com>
2021-09-01 21:39:51 +0300

    Ping pong contract

--- src/config.devnet.tsx
+++ src/config.devnet.tsx
@@ -5,12 +5,14 @@ export const decimals = 2;
 export const denomination = 18;
 export const gasPrice = 1000000000;
 export const version = 1;
+export const gasLimit = 50000;
+export const gasPerDataByte = 1500;
 
 export const walletConnectBridge = 'https://bridge.walletconnect.org';
 export const walletConnectDeepLink =
   'https://maiar.page.link/?apn=com.elrond.maiar.wallet&isi=1519405832&ibi=com.elrond.maiar.wallet.dev&link=https://maiar.com/';
 
-export const contractAddress = 'erd1qqqqqqqqqqqqqpgqp699jngundfqw07d8jzkepucvpzush6k3wvqyc44rx';
+export const contractAddress = 'erd1qqqqqqqqqqqqqpgquvt728n40ssd8n2qns9jrlqpwq2jc4rj4cysfuj3ad';
 
 export const network: Dapp.NetworkType = {
   id: 'devnet',
--- src/pages/Dashboard/Actions/index.tsx
+++ src/pages/Dashboard/Actions/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import axios from 'axios';
 import * as Dapp from '@elrondnetwork/dapp';
 import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,11 +8,63 @@ import { routeNames } from 'routes';
 import useNewTransaction from 'pages/Transaction/useNewTransaction';
 import { RawTransactionType } from 'helpers/types';
 import { network } from 'config';
+import { Address } from '@elrondnetwork/erdjs';
 
 const Actions = () => {
   const sendTransaction = Dapp.useSendTransaction();
   const { address } = Dapp.useContext();
   const newTransaction = useNewTransaction();
+  const [secondsLeft, setSecondsLeft] = React.useState<number>();
+  const [hasPing, setHasPing] = React.useState<boolean>();
+
+  const mount = () => {
+    if (secondsLeft) {
+      const interval = setInterval(() => {
+        setSecondsLeft((existing) => {
+          if (existing) {
+            return existing - 1;
+          } else {
+            clearInterval(interval);
+            return 0;
+          }
+        });
+      }, 1000);
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  };
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  React.useEffect(mount, [hasPing]);
+
+  React.useEffect(() => {
+    axios
+      .post(`${network.apiAddress}/vm-values/query`, {
+        scAddress: contractAddress,
+        funcName: 'getTimeToPong',
+        args: [Address.fromBech32(address).hex()],
+      })
+      .then(({ data: { data } }) => {
+        const [encoded] = data.data.returnData;
+
+        switch (encoded) {
+          case undefined:
+            setHasPing(true);
+            break;
+          case '':
+            setSecondsLeft(0);
+            setHasPing(true);
+            break;
+          default: {
+            const decoded = Buffer.from(encoded, 'base64').toString('hex');
+            setSecondsLeft(parseInt(decoded, 16));
+            setHasPing(false);
+            break;
+          }
+        }
+      });
+  }, []);
 
   const send = (transaction: RawTransactionType) => (e: React.MouseEvent) => {
     e.preventDefault();
@@ -21,45 +74,60 @@ const Actions = () => {
     });
   };
 
-  const basicTransaction: RawTransactionType = {
-    receiver: address,
-    value: '1',
+  const pongTransaction: RawTransactionType = {
+    receiver: contractAddress,
+    data: 'pong',
+    value: '0',
+    gasLimit: 10000000,
   };
 
-  const claimRewardsTransaction: RawTransactionType = {
+  const pingTransaction: RawTransactionType = {
     receiver: contractAddress,
-    data: 'claimRewards',
-    value: '0',
-    gasLimit: 250000000,
+    data: 'ping',
+    value: '1',
+    gasLimit: 10000000,
+  };
+
+  const pongAllowed = secondsLeft === 0;
+  const notAllowed = {
+    ...(pongAllowed ? {} : { cursor: 'not-allowed' }),
   };
 
   return (
     <div className="d-flex mt-4 justify-content-center">
-      <div className="action-btn">
-        <button className="btn" onClick={send(basicTransaction)}>
-          <FontAwesomeIcon icon={faArrowUp} className="text-primary" />
-        </button>
-        <a
-          href={routeNames.home}
-          onClick={send(basicTransaction)}
-          className="text-white text-decoration-none"
-        >
-          Send {network.egldLabel}
-        </a>
-      </div>
-
-      <div className="action-btn">
-        <button className="btn" onClick={send(claimRewardsTransaction)}>
-          <FontAwesomeIcon icon={faArrowDown} className="text-primary" />
-        </button>
-        <a
-          href="/"
-          onClick={send(claimRewardsTransaction)}
-          className="text-white text-decoration-none"
-        >
-          Claim rewards
-        </a>
-      </div>
+      {hasPing !== undefined && (
+        <>
+          {hasPing ? (
+            <div className="action-btn" onClick={send(pingTransaction)}>
+              <button className="btn">
+                <FontAwesomeIcon icon={faArrowUp} className="text-primary" />
+              </button>
+              <a href="/" className="text-white text-decoration-none">
+                Ping
+              </a>
+            </div>
+          ) : (
+            <div
+              className="action-btn"
+              onClick={pongAllowed ? send(pongTransaction) : () => {}}
+              style={notAllowed}
+            >
+              <button className={`btn ${!pongAllowed ? 'disabled' : ''}`} style={notAllowed}>
+                <FontAwesomeIcon icon={faArrowDown} className="text-primary" />
+              </button>
+              <span className="text-white">
+                {pongAllowed ? (
+                  <a href="/" className="text-white text-decoration-none">
+                    Pong
+                  </a>
+                ) : (
+                  <>{secondsLeft}s until Pong</>
+                )}
+              </span>
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
 };
