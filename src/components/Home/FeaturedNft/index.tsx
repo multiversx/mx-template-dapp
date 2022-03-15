2cc835fbd669cd867b1bb912ce9a10ed766c5150
sajib hasan <sajib001@gmail.com>
2022-03-15 10:36:55 +0600

    [add new branch] dev-nft

--- src/components/Home/FeaturedNft/index.tsx
+++ src/components/Home/FeaturedNft/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import NFTCard from 'components/Card/NFTCard';
+import { routeNames } from 'routes';
 import NFTImg1 from '../../../assets/img/nft-img/nft-crypto.gif';
 
 const data = [NFTImg1, NFTImg1, NFTImg1, NFTImg1];
@@ -13,7 +14,7 @@ const index = () => {
         <div>
           <h2>Featured NFTs</h2>
         </div>
-        <Link to='/nfts' className='link'>
+        <Link to={routeNames.nfts} className='link'>
           <p>View all NFTs</p>
         </Link>
       </div>
