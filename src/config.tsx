ca2826be499da892c1180d26f93e1497be77af09
Radu Mojic <radu.mojic@elrond.com>
2022-10-17 13:23:55 +0300

    [+] Enable WalletConnect 2 functionality using dapp-core 2.x with @elrondnetwork/erdjs-wallet-connect-provider 2.1.0-beta.1

--- src/config.tsx
+++ src/config.tsx
@@ -2,3 +2,6 @@ export const contractAddress =
   'erd1qqqqqqqqqqqqqpgq72l6vl07fkn3alyfq753mcy4nakm0l72396qkcud5x';
 
 export const dAppName = 'example Dapp';
+
+// Generate your own WalletConnect 2 ProjectId here: https://cloud.walletconnect.com/app
+export const walletConnectV2ProjectId = '9b1a9564f91cb659ffe21b73d5c4e2d8';
