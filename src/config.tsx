3fab5229e59c08d77d76051678be1eed93036cba
Tudor Morar <tudor.morar@elrond.com>
2022-10-21 15:37:41 +0300

    Extend config

--- src/config.tsx
+++ src/config.tsx
@@ -5,3 +5,6 @@ export const dAppName = 'example Dapp';
 
 // Generate your own WalletConnect 2 ProjectId here: https://cloud.walletconnect.com/app
 export const walletConnectV2ProjectId = '9b1a9564f91cb659ffe21b73d5c4e2d8';
+
+export const apiTimeout = 6000;
+export const transactionSize = 15;
