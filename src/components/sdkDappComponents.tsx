7fafdf6e6331e395d82201ee451e98d02df39916
arhtudormorar <tudor.morar@multiversx.com>
2023-03-28 15:46:48 +0300

    Upgrade sdk-dapp

--- src/components/sdkDappComponents.tsx
+++ src/components/sdkDappComponents.tsx
@@ -2,19 +2,19 @@
  * components get re-exported because it makes the build size smaller
  * and allows testing with Jest (see `moduleNameMapper` in package.json)
  */
-export { CopyButton } from '@multiversx/sdk-dapp/UI/CopyButton/index';
-export { ExtensionLoginButton } from '@multiversx/sdk-dapp/UI/extension/ExtensionLoginButton/index';
+export { CopyButton } from '@multiversx/sdk-dapp/UI/CopyButton/CopyButton';
+export { ExtensionLoginButton } from '@multiversx/sdk-dapp/UI/extension/ExtensionLoginButton/ExtensionLoginButton';
 export { FormatAmount } from '@multiversx/sdk-dapp/UI/FormatAmount/FormatAmount';
-export { LedgerLoginButton } from '@multiversx/sdk-dapp/UI/ledger/LedgerLoginButton/index';
-export { Loader } from '@multiversx/sdk-dapp/UI/Loader/index';
-export { NotificationModal } from '@multiversx/sdk-dapp/UI/NotificationModal/index';
-export { OperaWalletLoginButton } from '@multiversx/sdk-dapp/UI/operaWallet/OperaWalletLoginButton/index';
-export { PageState } from '@multiversx/sdk-dapp/UI/PageState/index';
+export { LedgerLoginButton } from '@multiversx/sdk-dapp/UI/ledger/LedgerLoginButton/LedgerLoginButton';
+export { Loader } from '@multiversx/sdk-dapp/UI/Loader/Loader';
+export { NotificationModal } from '@multiversx/sdk-dapp/UI/NotificationModal/NotificationModal';
+export { OperaWalletLoginButton } from '@multiversx/sdk-dapp/UI/operaWallet/OperaWalletLoginButton/OperaWalletLoginButton';
+export { PageState } from '@multiversx/sdk-dapp/UI/PageState/PageState';
 export { SignTransactionsModals } from '@multiversx/sdk-dapp/UI/SignTransactionsModals/SignTransactionsModals';
 export { TransactionsTable } from '@multiversx/sdk-dapp/UI/TransactionsTable/TransactionsTable';
 export { TransactionsToastList } from '@multiversx/sdk-dapp/UI/TransactionsToastList/TransactionsToastList';
-export { WalletConnectLoginButton } from '@multiversx/sdk-dapp/UI/walletConnect/WalletConnectLoginButton/index';
-export { WebWalletLoginButton } from '@multiversx/sdk-dapp/UI/webWallet/WebWalletLoginButton/index';
-export { AuthenticatedRoutesWrapper } from '@multiversx/sdk-dapp/wrappers/AuthenticatedRoutesWrapper/index';
+export { WalletConnectLoginButton } from '@multiversx/sdk-dapp/UI/walletConnect/WalletConnectLoginButton/WalletConnectLoginButton';
+export { WebWalletLoginButton } from '@multiversx/sdk-dapp/UI/webWallet/WebWalletLoginButton/WebWalletLoginButton';
+export { AuthenticatedRoutesWrapper } from '@multiversx/sdk-dapp/wrappers/AuthenticatedRoutesWrapper/AuthenticatedRoutesWrapper';
 export { AxiosInterceptorContext } from '@multiversx/sdk-dapp/wrappers/AxiosInterceptorContext/AxiosInterceptorContext';
-export { DappProvider } from '@multiversx/sdk-dapp/wrappers/DappProvider/index';
+export { DappProvider } from '@multiversx/sdk-dapp/wrappers/DappProvider/DappProvider';
