0b35221d868dc56dc2f0bbc7082dd51c005851da
Tudor Homoranu <tudorhomoranu@192-168-0-102.rdsnet.ro>
2023-06-12 19:22:28 +0300

    transaction flow added

--- cypress/e2e/SCTransactions/SCTransaction.cy.ts
+++ cypress/e2e/SCTransactions/SCTransaction.cy.ts
@@ -18,7 +18,6 @@ describe('Smart Contract Transactions', () => {
         cy.wait(180010);
         scTransaction(SCTransactionData.pong);
       } else {
-        // In this case will finish the existing flow with the pong transaction ,and after will proceed with a new ping-pong flow
         scTransaction(SCTransactionData.pong);
         scTransaction(SCTransactionData.ping);
         // Wait 3 minutes for the pong
