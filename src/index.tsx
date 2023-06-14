b7850485d791db1388ab3f8e2ba2c012334c3a4b
Alessandro <defranceschi_a@yahoo.com>
2023-06-14 13:14:10 +0200

    fix for css

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import './index.css';
-import './assets/sass/theme.scss';
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { App } from './App';
+import "./assets/sass/theme.scss";
+import "./index.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container as HTMLElement);
 root.render(<App />);
