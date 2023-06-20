321d2c80a75d53ed1fe904c9224fe3de48becad3
Ciprian Draghici <cipriandraghici93@gmail.com>
2023-06-20 15:49:02 +0300

    fix ledger login issue (#118)

--- src/pages/Dashboard/components/Actions/helpers/smartContract.ts
+++ src/pages/Dashboard/components/Actions/helpers/smartContract.ts
@@ -1,15 +1,8 @@
-import {
-  AbiRegistry,
-  SmartContractAbi,
-  SmartContract,
-  Address
-} from '@multiversx/sdk-core/out';
-
+import { AbiRegistry, SmartContract, Address } from '@multiversx/sdk-core/out';
 import { contractAddress } from 'config';
 import json from 'ping-pong.abi.json';
 
-const abiRegistry = AbiRegistry.create(json);
-const abi = new SmartContractAbi(abiRegistry);
+const abi = AbiRegistry.create(json);
 
 export const smartContract = new SmartContract({
   address: new Address(contractAddress),
