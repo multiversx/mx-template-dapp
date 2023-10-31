1a4246b6606c4f4bf5e762d1b8e256f16d18b2af
tudorgabriel <homoranu.tudor@gmail.com>
2023-10-31 12:20:15 +0200

    Th/e2e fix (#150)
    * base url updated

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'https://integration-template-dapp.multiversx.com',
+    baseUrl: 'https://integration.template-dapp.multiversx.com/',
     defaultCommandTimeout: 30000,
     responseTimeout: 70000,
     requestTimeout: 70000,
