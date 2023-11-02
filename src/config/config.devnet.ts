2f414bb1b63a9e5163c775071a5ce573fb162dc6
cipriandraghici <cipriandraghici93@gmail.com>
2023-11-02 10:31:54 +0200

    change devnet contract address

--- src/config/config.devnet.ts
+++ src/config/config.devnet.ts
@@ -3,7 +3,7 @@ import { EnvironmentsEnum } from 'types';
 export * from './sharedConfig';
 
 export const contractAddress =
-  'erd1qqqqqqqqqqqqqpgq72l6vl07fkn3alyfq753mcy4nakm0l72396qkcud5x';
+  'erd1qqqqqqqqqqqqqpgqm6ad6xrsjvxlcdcffqe8w58trpec09ug9l5qde96pq';
 export const API_URL = 'https://devnet-template-api.multiversx.com';
 export const sampleAuthenticatedDomains = [API_URL];
 export const environment = EnvironmentsEnum.devnet;
