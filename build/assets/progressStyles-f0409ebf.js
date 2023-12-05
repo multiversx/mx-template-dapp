22b87fba07813de1cb127fc22f0666fedf83a195
Tudor Morar <tudor.morar@multiversx.com>
2023-12-05 13:17:55 +0200

    Force build

---
+++ build/assets/progressStyles-f0409ebf.js
@@ -0,0 +1,15 @@
+import{v as e}from"./index-21aedfbb.js";e();var o=`.dapp-core-component__progressStyles__progress {
+  position: relative;
+}
+.dapp-core-component__progressStyles__progress .dapp-core-component__progressStyles__bar {
+  position: absolute;
+  pointer-events: none;
+  left: -0.5rem;
+  right: -0.5rem;
+  bottom: -0.5rem;
+  top: -0.5rem;
+  background-color: #495057;
+  opacity: 0.25;
+  transition: width 0.6s ease;
+  max-width: calc(100% + 1rem);
+}`;document.head.appendChild(document.createElement("style")).appendChild(document.createTextNode(o));var t={progress:"dapp-core-component__progressStyles__progress",bar:"dapp-core-component__progressStyles__bar"};export{o as css,t as default};
---
+++ build/assets/transactionDetails.styles-500b22de.js
@@ -0,0 +1,28 @@
+import{v as t}from"./index-21aedfbb.js";t();var e=`.dapp-core-component__transactionDetails-styles__title {
+  margin-bottom: 0;
+}
+
+.dapp-core-component__transactionDetails-styles__status {
+  font-size: 0.875rem;
+  margin: 0.25rem 0 0.5rem;
+  line-height: 1.5;
+}
+
+.dapp-core-component__transactionDetails-styles__container {
+  line-height: 1.5;
+  font-size: 0.875rem;
+  padding-right: 0.5rem;
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.2rem;
+}
+.dapp-core-component__transactionDetails-styles__container .dapp-core-component__transactionDetails-styles__icon {
+  font-size: 0.875rem;
+  color: #6c757d;
+  margin-right: 0.25rem;
+}
+.dapp-core-component__transactionDetails-styles__container .dapp-core-component__transactionDetails-styles__trim {
+  width: 11rem;
+  white-space: nowrap;
+  margin-right: 1rem;
+}`;document.head.appendChild(document.createElement("style")).appendChild(document.createTextNode(e));var a={title:"dapp-core-component__transactionDetails-styles__title",status:"dapp-core-component__transactionDetails-styles__status",container:"dapp-core-component__transactionDetails-styles__container",icon:"dapp-core-component__transactionDetails-styles__icon",trim:"dapp-core-component__transactionDetails-styles__trim"};export{e as css,a as default};
