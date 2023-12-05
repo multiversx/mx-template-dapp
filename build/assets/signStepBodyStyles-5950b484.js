64f2dfeb3caa2cdb423d3318a1092893cd76d0e6
Tudor Morar <tudor.morar@multiversx.com>
2023-12-05 11:35:59 +0200

    Force build

---
+++ build/assets/signStepBodyStyles-5950b484.js
@@ -0,0 +1,33 @@
+import{v as e}from"./index-7476174c.js";e();var o=`.dapp-core-component__signStepBodyStyles__summary {
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  width: 100%;
+}
+.dapp-core-component__signStepBodyStyles__summary .dapp-core-component__signStepBodyStyles__fields {
+  gap: 32px;
+  display: flex;
+  flex-direction: column;
+}
+.dapp-core-component__signStepBodyStyles__summary .dapp-core-component__signStepBodyStyles__fields .dapp-core-component__signStepBodyStyles__columns {
+  display: flex;
+  column-gap: 16px;
+  row-gap: 32px;
+  flex-wrap: wrap;
+  align-items: flex-start;
+}
+.dapp-core-component__signStepBodyStyles__summary .dapp-core-component__signStepBodyStyles__fields .dapp-core-component__signStepBodyStyles__columns .dapp-core-component__signStepBodyStyles__column {
+  min-width: calc(50% - 8px);
+  text-align: left;
+}
+.dapp-core-component__signStepBodyStyles__summary .dapp-core-component__signStepBodyStyles__buttons {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 72px;
+}
+@media (min-width: 768px) {
+  .dapp-core-component__signStepBodyStyles__summary .dapp-core-component__signStepBodyStyles__buttons {
+    margin-top: 80px;
+  }
+}`;document.head.appendChild(document.createElement("style")).appendChild(document.createTextNode(o));var t={summary:"dapp-core-component__signStepBodyStyles__summary",fields:"dapp-core-component__signStepBodyStyles__fields",columns:"dapp-core-component__signStepBodyStyles__columns",column:"dapp-core-component__signStepBodyStyles__column",buttons:"dapp-core-component__signStepBodyStyles__buttons"};export{o as css,t as default};
