f70ca3c18f637a2f6228b45b187f4465f16b1895
cipriandraghici <cipriandraghici93@gmail.com>
2023-12-08 09:53:13 +0200

    wip wallet hub integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from '@vitejs/plugin-react';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+// import basicSsl from '@vitejs/plugin-basic-ssl';
 import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import svgrPlugin from 'vite-plugin-svgr';
@@ -8,9 +8,14 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   server: {
     port: Number(process.env.PORT) || 3000,
+    headers: {
+      'Content-Security-Policy':
+        "frame-ancestors 'self' https://localhost:3002",
+      'allow-same-origin': 'true'
+    },
     strictPort: true,
     host: true,
-    https: true,
+    // https: true,
     watch: {
       usePolling: false,
       useFsEvents: false
@@ -21,7 +26,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    basicSsl(),
+    // basicSsl(),
     tsconfigPaths(),
     svgrPlugin(),
     nodePolyfills({
@@ -33,7 +38,7 @@ export default defineConfig({
   },
   preview: {
     port: 3002,
-    https: true,
+    // https: true,
     host: 'localhost',
     strictPort: true
   }
