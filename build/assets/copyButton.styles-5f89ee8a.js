3f85d61def773fc7896728669c84657393d91e0e
Tudor Morar <tudor.morar@multiversx.com>
2023-12-18 17:19:39 +0200

    Force build

---
+++ build/assets/copyButton.styles-5f89ee8a.js
@@ -0,0 +1,3 @@
+import{v as e}from"./index-f144cf3f.js";e();var o=`.dapp-core-component__copyButton-styles__copy {
+  color: #6c757d;
+}`;document.head.appendChild(document.createElement("style")).appendChild(document.createTextNode(o));var c={copy:"dapp-core-component__copyButton-styles__copy"};export{o as css,c as default};
---
+++ build/assets/scamPhishingStyles-f16a8c94.js
@@ -0,0 +1,44 @@
+import{v as i}from"./index-f144cf3f.js";i();var a=`.dapp-core-component__scamPhishingStyles__scam-phishing-alert {
+  margin: 40px auto 0;
+  font-size: 12px;
+  display: flex;
+  justify-content: center;
+  padding: 0 16px;
+  width: auto;
+  align-items: center;
+}
+.dapp-core-component__scamPhishingStyles__scam-phishing-alert .dapp-core-component__scamPhishingStyles__scam-phishing-alert-icon {
+  color: #1b46c2;
+  margin-right: 8px;
+}
+.dapp-core-component__scamPhishingStyles__scam-phishing-alert .dapp-core-component__scamPhishingStyles__scam-phishing-alert-text {
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+}
+@media (min-width: 768px) {
+  .dapp-core-component__scamPhishingStyles__scam-phishing-alert .dapp-core-component__scamPhishingStyles__scam-phishing-alert-text {
+    flex-direction: row;
+  }
+}
+.dapp-core-component__scamPhishingStyles__scam-phishing-alert .dapp-core-component__scamPhishingStyles__scam-phishing-alert-text strong {
+  color: #1b46c2;
+}
+@media (min-width: 768px) {
+  .dapp-core-component__scamPhishingStyles__scam-phishing-alert .dapp-core-component__scamPhishingStyles__scam-phishing-alert-text .dapp-core-component__scamPhishingStyles__scam-phishing-alert-prefix {
+    margin-left: 4px;
+  }
+}
+
+.dapp-core-component__scamPhishingStyles__scam-phishing-alert-authorization {
+  display: flex;
+  flex-direction: column;
+  margin-top: 0;
+}
+.dapp-core-component__scamPhishingStyles__scam-phishing-alert-authorization .dapp-core-component__scamPhishingStyles__scam-phishing-alert-text {
+  text-align: center;
+  flex-direction: row;
+}
+.dapp-core-component__scamPhishingStyles__scam-phishing-alert-authorization .dapp-core-component__scamPhishingStyles__scam-phishing-alert-authorization-duration {
+  margin-left: 4px;
+}`;document.head.appendChild(document.createElement("style")).appendChild(document.createTextNode(a));var t={"scam-phishing-alert":"dapp-core-component__scamPhishingStyles__scam-phishing-alert",scamPhishingAlert:"dapp-core-component__scamPhishingStyles__scam-phishing-alert","scam-phishing-alert-icon":"dapp-core-component__scamPhishingStyles__scam-phishing-alert-icon",scamPhishingAlertIcon:"dapp-core-component__scamPhishingStyles__scam-phishing-alert-icon","scam-phishing-alert-text":"dapp-core-component__scamPhishingStyles__scam-phishing-alert-text",scamPhishingAlertText:"dapp-core-component__scamPhishingStyles__scam-phishing-alert-text","scam-phishing-alert-prefix":"dapp-core-component__scamPhishingStyles__scam-phishing-alert-prefix",scamPhishingAlertPrefix:"dapp-core-component__scamPhishingStyles__scam-phishing-alert-prefix","scam-phishing-alert-authorization":"dapp-core-component__scamPhishingStyles__scam-phishing-alert-authorization",scamPhishingAlertAuthorization:"dapp-core-component__scamPhishingStyles__scam-phishing-alert-authorization","scam-phishing-alert-authorization-duration":"dapp-core-component__scamPhishingStyles__scam-phishing-alert-authorization-duration",scamPhishingAlertAuthorizationDuration:"dapp-core-component__scamPhishingStyles__scam-phishing-alert-authorization-duration"};export{a as css,t as default};
