d65c41fb22d85e3332c9e227fcf2375add739780
Tudor Morar <tudor.morar@multiversx.com>
2023-12-27 09:56:57 +0200

    Force past version

---
+++ build/assets/tokenDetailsStyles-e6b77872.js
@@ -0,0 +1,3 @@
+import{v as e}from"./index-7476174c.js";e();var o=`.dapp-core-component__tokenDetailsStyles__token-symbol-custom-token {
+  color: inherit;
+}`;document.head.appendChild(document.createElement("style")).appendChild(document.createTextNode(o));var n={"token-symbol-custom-token":"dapp-core-component__tokenDetailsStyles__token-symbol-custom-token",tokenSymbolCustomToken:"dapp-core-component__tokenDetailsStyles__token-symbol-custom-token"};export{o as css,n as default};
