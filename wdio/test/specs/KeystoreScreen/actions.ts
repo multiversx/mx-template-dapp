6c90743fd7e63d4214b86b2259a8aa0b2da84123
Tudor Homoranu <tudor.homoranu@multiversx.com>
2024-03-22 11:02:33 +0200

    package updated

--- wdio/test/specs/KeystoreScreen/actions.ts
+++ wdio/test/specs/KeystoreScreen/actions.ts
@@ -9,31 +9,4 @@ import {
   validateTransaction
 } from '../../utils/actions.ts';
 
-export const scTransaction = async (type: string) => {
-  const btn = await $(`[data-testid="${type}"]`);
-  await browser.pause(1500);
-  if (await btn.getAttribute('disabled')) {
-    return;
-  } else {
-    await browser.pause(1500);
-    await $(btn).click();
-    await browser.pause(3000);
-    await browser.switchWindow(GlobalDataEnum.walletWindow);
-    await confirmPass();
-    await $(GlobalSelectorEnum.signBtn).click();
-    await browser.pause(500);
-    await browser.switchWindow(GlobalDataEnum.daapWindow);
-    await validateToast(GlobalSelectorEnum.toastSelector);
-    await validateTransaction(TransactionIndexEnum.ping);
-  }
-};
 
-export const pingPongHandler = async (type: string) => {
-  const btn = await $(`[data-testid="btnPing${type}"]`);
-  await browser.pause(1500);
-  if (await btn.getAttribute('disabled')) {
-    await scTransaction(`btnPong${type}`);
-  } else {
-    await scTransaction(`btnPing${type}`);
-  }
-};
