6bdd270f71126585e83760facb8bc6b31a6ce37c
Tudor Homoranu <tudor.homoranu@multiversx.com>
2024-03-29 11:01:10 +0200

    remove firefox

--- wdio/wdio.conf.ts
+++ wdio/wdio.conf.ts
@@ -15,7 +15,7 @@ export const config: Options.Testrunner = {
   specs: ['./test/specs/**/*.ts'],
   suites: {
     smoke: [
-      // './test/specs/PemScreen/PemSpec.e2e.ts'
+      './test/specs/PemScreen/PemSpec.e2e.ts',
       './test/specs/KeystoreScreen/Keystore.e2e.ts'
     ]
   },
@@ -33,15 +33,12 @@ export const config: Options.Testrunner = {
         args: [
           '--disable-infobars',
           '--start-maximized',
-          '--no-sandbox'
-          // '--headless=new'
+          '--no-sandbox',
+          '--headless=new'
         ]
       },
       acceptInsecureCerts: true
     }
-    // {
-    //   browserName: 'firefox'
-    // }
   ],
 
   logLevel: 'error',
