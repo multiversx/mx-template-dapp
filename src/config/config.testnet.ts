a976c89cbac1d50e4262fc8a63e9b3dd1c951d65
Radu Mojic <radu.mojic@multiversx.com>
2024-06-26 14:10:21 +0300

    updated testnet ping-pong contract address

--- src/config/config.testnet.ts
+++ src/config/config.testnet.ts
@@ -3,7 +3,7 @@ import { EnvironmentsEnum } from 'types';
 export * from './sharedConfig';
 
 export const contractAddress =
-  'erd1qqqqqqqqqqqqqpgqq3qsdxf55rlz5ka8mw3jdnacm8dlkuy09l5ql0wrlm';
+  'erd1qqqqqqqqqqqqqpgqfu2wwk9vjd6rnvu38vyushh67dtdkuvy9l5q3wkk6r';
 export const API_URL = 'https://testnet-template-api.multiversx.com';
 export const sampleAuthenticatedDomains = [API_URL];
 export const environment = EnvironmentsEnum.testnet;
