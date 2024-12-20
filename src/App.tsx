87c66ec9b42ba72ecd36bf76707cc2951aad0ecb
Miro Mărgineanu <miro.margineanu@multiversx.com>
2024-12-20 16:32:13 +0200

    Updated code structure.

--- src/App.tsx
+++ src/App.tsx
@@ -22,68 +22,63 @@ import { PageNotFound, Unlock } from 'pages';
 import { routes } from 'routes';
 import { BatchTransactionsContextProvider } from 'wrappers';
 
-const AppContent = () => {
-  return (
-    <DappProvider
-      environment={environment}
-      customNetworkConfig={{
-        name: 'customConfig',
-        apiTimeout,
-        walletConnectV2ProjectId
-      }}
-      dappConfig={{
-        shouldUseWebViewProvider: true,
-        logoutRoute: RouteNamesEnum.unlock
-      }}
-      customComponents={{
-        transactionTracker: {
-          // uncomment this to use the custom transaction tracker
-          // component: TransactionsTracker,
-          props: {
-            onSuccess: (sessionId: string) => {
-              console.log(`Session ${sessionId} successfully completed`);
-            },
-            onFail: (sessionId: string, errorMessage: string) => {
-              console.log(`Session ${sessionId} failed. ${errorMessage ?? ''}`);
-            }
+const AppContent = () => (
+  <DappProvider
+    environment={environment}
+    customNetworkConfig={{
+      name: 'customConfig',
+      apiTimeout,
+      walletConnectV2ProjectId
+    }}
+    dappConfig={{
+      shouldUseWebViewProvider: true,
+      logoutRoute: RouteNamesEnum.unlock
+    }}
+    customComponents={{
+      transactionTracker: {
+        // uncomment this to use the custom transaction tracker
+        // component: TransactionsTracker,
+        props: {
+          onSuccess: (sessionId: string) => {
+            console.log(`Session ${sessionId} successfully completed`);
+          },
+          onFail: (sessionId: string, errorMessage: string) => {
+            console.log(`Session ${sessionId} failed. ${errorMessage ?? ''}`);
           }
         }
-      }}
+      }
+    }}
+  >
+    <AxiosInterceptorContext.Listener>
+      <Layout>
+        <TransactionsToastList />
+        <NotificationModal />
+        <SignTransactionsModals />
+        <Routes>
+          <Route path={RouteNamesEnum.unlock} element={<Unlock />} />
+          {routes.map((route) => (
+            <Route
+              path={route.path}
+              key={`route-key-'${route.path}`}
+              element={<route.component />}
+            />
+          ))}
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </Layout>
+    </AxiosInterceptorContext.Listener>
+  </DappProvider>
+);
+export const App = () => (
+  <AxiosInterceptorContext.Provider>
+    <AxiosInterceptorContext.Interceptor
+      authenticatedDomains={sampleAuthenticatedDomains}
     >
-      <AxiosInterceptorContext.Listener>
-        <Layout>
-          <TransactionsToastList />
-          <NotificationModal />
-          <SignTransactionsModals />
-          <Routes>
-            <Route path={RouteNamesEnum.unlock} element={<Unlock />} />
-            {routes.map((route) => (
-              <Route
-                path={route.path}
-                key={`route-key-'${route.path}`}
-                element={<route.component />}
-              />
-            ))}
-            <Route path='*' element={<PageNotFound />} />
-          </Routes>
-        </Layout>
-      </AxiosInterceptorContext.Listener>
-    </DappProvider>
-  );
-};
-
-export const App = () => {
-  return (
-    <AxiosInterceptorContext.Provider>
-      <AxiosInterceptorContext.Interceptor
-        authenticatedDomains={sampleAuthenticatedDomains}
-      >
-        <Router>
-          <BatchTransactionsContextProvider>
-            <AppContent />
-          </BatchTransactionsContextProvider>
-        </Router>
-      </AxiosInterceptorContext.Interceptor>
-    </AxiosInterceptorContext.Provider>
-  );
-};
+      <Router>
+        <BatchTransactionsContextProvider>
+          <AppContent />
+        </BatchTransactionsContextProvider>
+      </Router>
+    </AxiosInterceptorContext.Interceptor>
+  </AxiosInterceptorContext.Provider>
+);
