efb34824be523dc21b4da4f2ad4cb6483da46a6e
eminesa <eminesa34@gmail.com>
2024-12-28 00:23:27 +0300

    Added sc address

--- src/config/config.devnet.ts
+++ src/config/config.devnet.ts
@@ -3,7 +3,7 @@ import { EnvironmentsEnum } from 'types';
 export * from './sharedConfig';
 
 export const contractAddress =
-  'erd1qqqqqqqqqqqqqpgqm6ad6xrsjvxlcdcffqe8w58trpec09ug9l5qde96pq';
+  'erd1qqqqqqqqqqqqqpgq06sxx4zhattlzn4vkfz9rcmak9usrfajj02qdtugnv';
 export const API_URL = 'https://devnet-template-api.multiversx.com';
 export const sampleAuthenticatedDomains = [API_URL];
 export const environment = EnvironmentsEnum.devnet;
