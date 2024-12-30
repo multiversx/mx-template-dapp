34b4eb7045417a3a6046bba742fba62b0d251548
razvan.tomegea <razvan.tomegea@gmail.com>
2024-12-30 17:51:48 +0200

    Removed logs

--- src/pages/Unlock/Unlock.tsx
+++ src/pages/Unlock/Unlock.tsx
@@ -2,27 +2,11 @@ import { AuthRedirectWrapper } from 'wrappers';
 import { ProviderTypeEnum } from 'types';
 import { Button } from 'components';
 import { useNavigate } from 'react-router-dom';
-import {
-  accountSelector,
-  networkSelector,
-  ProviderFactory,
-  setCustomWalletAddress
-} from 'lib/sdkDappCore';
-import { useSelector } from 'hooks/useSelector';
+import { ProviderFactory } from 'lib/sdkDappCore';
 import { RouteNamesEnum } from 'localConstants/routes';
-import { useEffect } from 'react';
 
 export const Unlock = () => {
-  // In React it can be done like this
-  const CWA = useSelector(networkSelector);
-  const account = useSelector(accountSelector);
-
   const navigate = useNavigate();
-
-  useEffect(() => {
-    debugger;
-    console.log(112, account);
-  }, [account]);
 
   const handleLogin = (type: ProviderTypeEnum) => async () => {
     const config = {
@@ -30,26 +14,12 @@ export const Unlock = () => {
     };
 
     const provider = await ProviderFactory.create(config);
-    const data = await provider?.login();
-    console.log('data', data);
-    debugger;
+    await provider?.login();
     navigate(RouteNamesEnum.dashboard);
   };
 
   return (
     <AuthRedirectWrapper requireAuth={false}>
-      <>
-        <h1>Demo</h1>
-        <div className='card'>
-          <Button onClick={() => setCustomWalletAddress(Date.now().toString())}>
-            setCustomWalletAddress: {CWA.customWalletAddress}
-          </Button>
-        </div>
-
-        <p className='read-the-docs'>
-          Click on the Vite and React logos to learn more
-        </p>
-      </>
       <div className='flex justify-center items-center'>
         <div
           className='flex flex-col p-6 items-center justify-center gap-4 rounded-xl bg-[#f6f8fa]'
--- src/wrappers/AuthRedirectWrapper/AuthRedirectWrapper.tsx
+++ src/wrappers/AuthRedirectWrapper/AuthRedirectWrapper.tsx
@@ -2,7 +2,6 @@ import { PropsWithChildren, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { RouteNamesEnum } from 'localConstants';
 import { isLoggedInSelector } from 'lib/sdkDappCore';
-import { addressSelector } from '@multiversx/sdk-dapp-core/out/store/selectors/accountSelectors';
 import { useSelector } from 'hooks/useSelector';
 
 interface AuthRedirectWrapperPropsType extends PropsWithChildren {
@@ -14,21 +13,16 @@ export const AuthRedirectWrapper = ({
   requireAuth = true
 }: AuthRedirectWrapperPropsType) => {
   const isLoggedIn = useSelector(isLoggedInSelector);
-  const address = useSelector(addressSelector);
   const navigate = useNavigate();
 
   useEffect(() => {
-    console.log(address, isLoggedIn);
     if (isLoggedIn && !requireAuth) {
-      debugger;
       navigate(RouteNamesEnum.dashboard);
 
       return;
     }
 
     if (!isLoggedIn && requireAuth) {
-      debugger;
-
       navigate(RouteNamesEnum.unlock);
     }
   }, [isLoggedIn]);
