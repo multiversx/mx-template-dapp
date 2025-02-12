87db6090bede9ba674fe5e41596d539f269bf8e0
razvan.tomegea <razvan.tomegea@gmail.com>
2025-02-12 15:59:57 +0200

    Refactor

--- src/components/ContractAddress/ContractAddress.tsx
+++ src/components/ContractAddress/ContractAddress.tsx
@@ -1,6 +1,6 @@
 import { Label } from 'components/Label';
 import { contractAddress } from 'config';
-import { ACCOUNTS_ENDPOINT } from 'localConstants';
+import { ACCOUNTS_ENDPOINT } from 'localConstants/sdkDappCoreConstants';
 import { ExplorerLink } from '../ExplorerLink';
 
 export const ContractAddress = () => {
