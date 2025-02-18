7e4079b30581279be73896c811379e6a1c86a20a
razvan.tomegea <razvan.tomegea@gmail.com>
2025-02-18 17:27:28 +0200

    Added axios interceptors

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { PageNotFound } from 'pages/PageNotFound/PageNotFound';
 import { routes } from 'routes';
-import { BatchTransactionsContextProvider } from 'wrappers';
+import { AxiosInterceptors, BatchTransactionsContextProvider } from 'wrappers';
 import { Layout } from './components';
 
 export const App = () => {
   return (
     <Router>
-      <BatchTransactionsContextProvider>
-        <Layout>
-          <Routes>
-            {routes.map((route) => (
-              <Route
-                key={`route-key-'${route.path}`}
-                path={route.path}
-                element={<route.component />}
-              />
-            ))}
-            <Route path='*' element={<PageNotFound />} />
-          </Routes>
-        </Layout>
-      </BatchTransactionsContextProvider>
+      <AxiosInterceptors>
+        <BatchTransactionsContextProvider>
+          <Layout>
+            <Routes>
+              {routes.map((route) => (
+                <Route
+                  key={`route-key-'${route.path}`}
+                  path={route.path}
+                  element={<route.component />}
+                />
+              ))}
+              <Route path='*' element={<PageNotFound />} />
+            </Routes>
+          </Layout>
+        </BatchTransactionsContextProvider>
+      </AxiosInterceptors>
     </Router>
   );
 };
