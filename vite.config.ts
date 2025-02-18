50271daca31f8ab557ea9cbde556b2f489724622
Danut <daniel.ilie1997@gmail.com>
2025-02-18 13:40:11 +0200

    added sdk-dapp-core-ui to optimizeDeps

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
-import react from '@vitejs/plugin-react';
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import svgrPlugin from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  //TODO remove this when excluded from optional dependencies
+  optimizeDeps: {
+    exclude: ['@multiversx/sdk-dapp-core-ui'] // Prevent Vite from trying to pre-bundle it
+  },
   server: {
     port: Number(process.env.PORT) || 3000,
     strictPort: true,
