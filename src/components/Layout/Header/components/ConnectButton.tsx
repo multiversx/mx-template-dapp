b6cf275a6b125714b8a2ca47d1fd80b2cc0c50a2
Tudor Morar <tudor.morar@multiversx.com>
2025-02-21 17:31:24 +0200

    Hide UnlockButton

--- src/components/Layout/Header/components/ConnectButton.tsx
+++ src/components/Layout/Header/components/ConnectButton.tsx
@@ -1,10 +1,12 @@
-import { Button } from 'components';
-import { useState } from 'react';
-import { UnlockPanel, UnlockButton, ProviderFactory } from 'lib';
-import { ExtendedProviders } from 'initConfig';
 import { IProviderFactory } from '@multiversx/sdk-dapp-core/out/core/providers/types/providerFactory.types';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Button } from 'components';
+import { ExtendedProviders } from 'initConfig';
+import { ProviderFactory, UnlockButton, UnlockPanel } from 'lib';
 import { RouteNamesEnum } from 'localConstants';
+
+const SHOW_ADVANCED_LOGIN_METHOD = false;
 
 export const ConnectButton = () => {
   const [open, setOpen] = useState(false);
@@ -34,14 +36,19 @@ export const ConnectButton = () => {
           setOpen(false);
         }}
       >
-        <UnlockButton
-          label='In Memory Provider'
-          onClick={() =>
-            handleLogin({
-              type: ExtendedProviders.inMemoryProvider
-            })
-          }
-        />
+        {
+          // you can safely remove this if you don't need to implement a custom provider
+          SHOW_ADVANCED_LOGIN_METHOD && (
+            <UnlockButton
+              label='In Memory Provider'
+              onClick={() =>
+                handleLogin({
+                  type: ExtendedProviders.inMemoryProvider
+                })
+              }
+            />
+          )
+        }
       </UnlockPanel>
     </>
   );
