079d73fe6fa093b76344bc50bac89ec666c1ef27
Radu Mojic <radu.mojic@multiversx.com>
2025-05-01 20:25:59 +0300

    update api endpoint

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -75,7 +75,7 @@ const WIDGETS: WidgetType[] = [
     widget: Transactions,
     description: 'List transactions for the connected account',
     reference:
-      'https://api.elrond.com/#/accounts/AccountController_getAccountTransactions'
+      'https://api.multiversx.com/#/accounts/AccountController_getAccountTransactions'
   },
   {
     title: 'Transactions (Ping & Pong)',
@@ -83,7 +83,7 @@ const WIDGETS: WidgetType[] = [
     props: { receiver: contractAddress },
     description: 'List transactions filtered for a given Smart Contract',
     reference:
-      'https://api.elrond.com/#/accounts/AccountController_getAccountTransactions'
+      'https://api.multiversx.com/#/accounts/AccountController_getAccountTransactions'
   }
 ];
 
