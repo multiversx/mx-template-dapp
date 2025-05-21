c7b9bc68af1cda894da31d73be5a4ec09b982427
DanutIlie <42973343+DanutIlie@users.noreply.github.com>
2025-05-21 11:24:11 +0300

    update sdk-dapp-packages (#338)

--- src/lib/sdkDapp/sdkDapp.types.ts
+++ src/lib/sdkDapp/sdkDapp.types.ts
@@ -1,14 +1,14 @@
-export type { AccountType } from '@multiversx/sdk-dapp-core/out/types/account.types';
-export { EnvironmentsEnum } from '@multiversx/sdk-dapp-core/out/types/enums.types';
-export type { FormatAmountControllerPropsType } from '@multiversx/sdk-dapp-core/out/controllers/FormatAmountController/types';
-export type { ICustomProvider } from '@multiversx/sdk-dapp-core/out/core/providers/types/providerFactory.types';
-export type { IProvider } from '@multiversx/sdk-dapp-core/out/core/providers/types/providerFactory.types';
-export type { IProviderFactory } from '@multiversx/sdk-dapp-core/out/core/providers/types/providerFactory.types';
-export type { InitAppType } from '@multiversx/sdk-dapp-core/out/core/methods/initApp/initApp.types';
+export type { AccountType } from '@multiversx/sdk-dapp/out/types/account.types';
+export { EnvironmentsEnum } from '@multiversx/sdk-dapp/out/types/enums.types';
+export type { FormatAmountControllerPropsType } from '@multiversx/sdk-dapp/out/controllers/FormatAmountController/types';
+export type { ICustomProvider } from '@multiversx/sdk-dapp/out/core/providers/types/providerFactory.types';
+export type { IProvider } from '@multiversx/sdk-dapp/out/core/providers/types/providerFactory.types';
+export type { IProviderFactory } from '@multiversx/sdk-dapp/out/core/providers/types/providerFactory.types';
+export type { InitAppType } from '@multiversx/sdk-dapp/out/core/methods/initApp/initApp.types';
 export type {
   InterpretedTransactionType,
   ServerTransactionType
-} from '@multiversx/sdk-dapp-core/out/types/serverTransactions.types';
-export { ProviderTypeEnum } from '@multiversx/sdk-dapp-core/out/core/providers/types/providerFactory.types';
-export type { SignedTransactionType } from '@multiversx/sdk-dapp-core/out/types/transactions.types';
-export type { TransactionsDisplayInfoType } from '@multiversx/sdk-dapp-core/out/types/transactions.types';
+} from '@multiversx/sdk-dapp/out/types/serverTransactions.types';
+export { ProviderTypeEnum } from '@multiversx/sdk-dapp/out/core/providers/types/providerFactory.types';
+export type { SignedTransactionType } from '@multiversx/sdk-dapp/out/types/transactions.types';
+export type { TransactionsDisplayInfoType } from '@multiversx/sdk-dapp/out/types/transactions.types';
