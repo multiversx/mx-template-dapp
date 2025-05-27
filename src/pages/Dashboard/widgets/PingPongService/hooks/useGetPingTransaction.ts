9e2c98f34c6cc6ee3a251fa5bb89c99893cfd581
Tudor Morar <tudor.morar@multiversx.com>
2025-05-27 10:40:47 +0300

    Fix useGetPingTransaction

--- src/pages/Dashboard/widgets/PingPongService/hooks/useGetPingTransaction.ts
+++ src/pages/Dashboard/widgets/PingPongService/hooks/useGetPingTransaction.ts
@@ -1,19 +1,26 @@
 import axios from 'axios';
 import { API_URL } from 'config';
-import { Transaction } from 'lib';
+import { Transaction, useGetLoginInfo } from 'lib';
 
 export const useGetPingTransaction = () => {
+  const { tokenLogin } = useGetLoginInfo();
+
   return async () => {
     try {
-      const { data } = await axios.post<Transaction>(
+      const { data } = await axios.post(
         '/ping-pong/abi/ping',
         {},
         {
-          baseURL: API_URL
+          baseURL: API_URL,
+          headers: {
+            Authorization: `Bearer ${tokenLogin?.nativeAuthToken}`
+          }
         }
       );
 
-      return data;
+      const pingTransaction = Transaction.newFromPlainObject(data);
+
+      return pingTransaction;
     } catch (err) {
       console.error('Unable to get Ping Transaction', err);
       return null;
