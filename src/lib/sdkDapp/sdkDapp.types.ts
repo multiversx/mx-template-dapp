25ce4d2e70f76d067c5471cff2632913912a3e53
Tudor Morar <tudor.morar@multiversx.com>
2025-05-27 12:56:18 +0300

    Remove unused exports

--- src/lib/sdkDapp/sdkDapp.types.ts
+++ src/lib/sdkDapp/sdkDapp.types.ts
@@ -1,14 +1,9 @@
 export type { AccountType } from '@multiversx/sdk-dapp/out/types/account.types';
 export { EnvironmentsEnum } from '@multiversx/sdk-dapp/out/types/enums.types';
-export type { FormatAmountControllerPropsType } from '@multiversx/sdk-dapp/out/controllers/FormatAmountController/types';
 export type { ICustomProvider } from '@multiversx/sdk-dapp/out/providers/types/providerFactory.types';
 export type { IProvider } from '@multiversx/sdk-dapp/out/providers/types/providerFactory.types';
-export type { IProviderFactory } from '@multiversx/sdk-dapp/out/providers/types/providerFactory.types';
 export type { InitAppType } from '@multiversx/sdk-dapp/out/methods/initApp/initApp.types';
-export type {
-  InterpretedTransactionType,
-  ServerTransactionType
-} from '@multiversx/sdk-dapp/out/types/serverTransactions.types';
 export { ProviderTypeEnum } from '@multiversx/sdk-dapp/out/providers/types/providerFactory.types';
+export type { ServerTransactionType } from '@multiversx/sdk-dapp/out/types/serverTransactions.types';
 export type { SignedTransactionType } from '@multiversx/sdk-dapp/out/types/transactions.types';
 export type { TransactionsDisplayInfoType } from '@multiversx/sdk-dapp/out/types/transactions.types';
