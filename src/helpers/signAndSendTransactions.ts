83f9233c5d7d85c6ca08508148bbcd7cafc26e15
Tudor Morar <tudor.morar@multiversx.com>
2025-06-26 11:53:51 +0300

    Remove unused

--- src/helpers/signAndSendTransactions.ts
+++ src/helpers/signAndSendTransactions.ts
@@ -20,13 +20,7 @@ export const signAndSendTransactions = async ({
   const signedTransactions = await provider.signTransactions(transactions);
   const sentTransactions = await txManager.send(signedTransactions);
   const sessionId = await txManager.track(sentTransactions, {
-    transactionsDisplayInfo,
-    onSuccess: async (id) => {
-      console.log('onSuccess -->', id);
-    },
-    onFail: async (id) => {
-      console.log('onFail -->', id);
-    }
+    transactionsDisplayInfo
   });
 
   return sessionId;
