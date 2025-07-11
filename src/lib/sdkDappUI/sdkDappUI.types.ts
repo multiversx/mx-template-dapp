6a5117a2c3cff0d56c6b8b1e96511f2ab80ca7ed
Tudor Morar <tudor.morar@multiversx.com>
2025-07-11 10:19:54 +0300

    Remove unused

--- src/lib/sdkDappUI/sdkDappUI.types.ts
+++ src/lib/sdkDappUI/sdkDappUI.types.ts
@@ -1,4 +1,3 @@
 export type { MvxCopyButton as MvxCopyButtonPropsType } from '@multiversx/sdk-dapp-ui/web-components/mvx-copy-button';
 export type { MvxExplorerLink as MvxExplorerLinkPropsType } from '@multiversx/sdk-dapp-ui/web-components/mvx-explorer-link';
 export type { MvxFormatAmount as MvxFormatAmountPropsType } from '@multiversx/sdk-dapp-ui/web-components/mvx-format-amount';
-export type { MvxTransactionsTable as MvxTransactionsTablePropsType } from '@multiversx/sdk-dapp-ui/web-components/mvx-transactions-table';
