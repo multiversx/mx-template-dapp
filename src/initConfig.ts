a0d7a618b4e8260e2b408dfceebcfba20319d667
Iulia Cimpeanu <iuliaalexandra.ignat@gmail.com>
2025-07-18 12:25:06 +0300

    Disabled in memory provider

--- src/initConfig.ts
+++ src/initConfig.ts
@@ -1,21 +1,20 @@
 import './styles/globals.css';
 
 import { walletConnectV2ProjectId } from 'config';
-import { EnvironmentsEnum, ICustomProvider, InitAppType } from './lib';
-import { InMemoryProvider } from './provider/inMemoryProvider';
+import { EnvironmentsEnum, InitAppType } from './lib';
 
-const providers: ICustomProvider[] = [
-  {
-    name: 'In Memory Provider',
-    type: 'inMemoryProvider',
-    iconUrl: `${window.location.origin}/multiversx-white.svg`,
-    constructor: async (options) => new InMemoryProvider(options)
-  }
-];
+// const providers: ICustomProvider[] = [
+//   {
+//     name: 'In Memory Provider',
+//     type: 'inMemoryProvider',
+//     iconUrl: `${window.location.origin}/multiversx-white.svg`,
+//     constructor: async (options) => new InMemoryProvider(options)
+//   }
+// ];
 
 (window as any).multiversx = {};
 // Option 1: Add providers using the `window.providers` array
-(window as any).multiversx.providers = providers;
+// (window as any).multiversx.providers = providers;
 
 export const config: InitAppType = {
   storage: { getStorageCallback: () => sessionStorage },
