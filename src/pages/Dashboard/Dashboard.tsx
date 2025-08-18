06fd2ef5d94c5cc1bc5143ff7d58a354d575149d
Iulia Cimpeanu <iuliaalexandra.ignat@gmail.com>
2025-08-18 12:31:57 +0300

    Fixed left panel

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -21,7 +21,7 @@ const styles = {
   mobilePanelContainer: 'mobile-panel-container fixed bottom-0 left-0 right-0 z-50 max-h-full overflow-y-auto lg:static lg:max-h-none lg:overflow-visible',
   desktopPanelContainer: 'desktop-panel-container lg:flex',
   dashboardContent: 'dashboard-content flex flex-col gap-6 justify-center items-center flex-1 w-full overflow-auto border-l border-secondary p-4 lg:p-6 transition-all duration-300',
-  dashboardContentMobilePanelOpen: 'dashboard-content-mobile-panel-open opacity-20 pointer-events-none',
+  dashboardContentMobilePanelOpen: 'dashboard-content-mobile-panel-open opacity-20 lg:opacity-100 pointer-events-none',
   dashboardWidgets: 'dashboard-widgets flex flex-col gap-6  w-full'
 } satisfies Record<string, string>;
 
