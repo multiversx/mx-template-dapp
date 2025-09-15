e1eacb90d936f81310a7e9cd3c1830819b6e5d06
Iulia Cimpeanu <iuliaalexandra.ignat@gmail.com>
2025-09-15 11:10:07 +0300

    Updated styles

--- src/components/MissingNativeAuthError/MissingNativeAuthError.tsx
+++ src/components/MissingNativeAuthError/MissingNativeAuthError.tsx
@@ -3,8 +3,8 @@ import { OutputContainer } from '../OutputContainer';
 // prettier-ignore
 const styles = {
   errorContainer: 'error-container flex items-center gap-1',
-  emphasizedText: 'emphasized-text ml-1 mr-1 inline-block px-2 py-1 text-sm font-semibold leading-none bg-red-500 text-white rounded',
-  nativeAuthText: 'native-auth-text ml-1 mr-1 inline-block px-2 py-1 text-sm font-semibold leading-none bg-primary text-primary rounded transition-all duration-200 ease-out'
+  emphasizedText: 'emphasized-text text-red-500 font-bold',
+  nativeAuthText: 'native-auth-text font-bold italic leading-none text-primary transition-all duration-200 ease-out'
 } satisfies Record<string, string>;
 
 export const MissingNativeAuthError = () => (
@@ -12,9 +12,9 @@ export const MissingNativeAuthError = () => (
     <div className={styles.errorContainer}>
       <p>
         Information could
-        <span className={styles.emphasizedText}>NOT</span>
+        <span className={styles.emphasizedText}> NOT </span>
         be displayed because
-        <span className={styles.nativeAuthText}>nativeAuth</span>
+        <span className={styles.nativeAuthText}> nativeAuth </span>
         is not active
       </p>
     </div>
--- src/components/OutputContainer/components/TransactionOutput.tsx
+++ src/components/OutputContainer/components/TransactionOutput.tsx
@@ -17,8 +17,8 @@ import {
 const styles = {
   transactionContainer: 'transaction-container flex flex-col',
   transactionElementContainer: 'transaction-elem-container flex gap-2',
-  transactionElement: 'transaction-elem flex justify-between w-full',
-  buttons: 'buttons flex gap-3',
+  transactionElement: 'transaction-elem flex gap-3 w-full',
+  buttons: 'buttons flex gap-2',
   dataContainer: 'data-container whitespace-nowrap'
 } satisfies Record<string, string>;
 
