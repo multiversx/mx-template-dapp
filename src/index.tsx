5747617504ed56d2eb83bc34aa3e87ce9471019c
Tudor Morar <tudor.morar@multiversx.com>
2025-09-22 10:57:50 +0300

    Work on package size

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
-import { defineCustomElements } from '@multiversx/sdk-dapp-ui';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { initApp } from 'lib';
 import { App } from './App';
 import { config } from './initConfig';
-
-defineCustomElements();
 
 initApp(config).then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
