04de13a51c29df7b814b3596fbc6c8820527da98
Roland Moldovan <roland.moldovan@multiversx.com>
2025-10-13 09:44:08 +0300

    debug connection error

--- tests/support/navigateToConnectWallet.ts
+++ tests/support/navigateToConnectWallet.ts
@@ -1,22 +1,7 @@
 import { Page } from '@playwright/test';
 
 export const navigateToConnectWallet = async (page: Page) => {
-  page.setDefaultNavigationTimeout(10 * 1000); // 10 seconds
-  // Sometimes the navigation fails, so we retry it
-  let retries = 3;
-  while (retries > 0) {
-    try {
-      await page.goto('/', { waitUntil: 'load', timeout: 30000 });
-      break;
-    } catch (error) {
-      retries--;
-      if (retries === 0) {
-        throw error;
-      }
-      console.log(`Navigation failed, retrying... (${retries} attempts left)`);
-      // await page.waitForTimeout(2000);
-    }
-  }
-
+  await page.goto('/', { waitUntil: 'load', timeout: 30000 });
+  expect(await page.getByRole('button', { name: 'Connect' }).first());
   await page.getByRole('button', { name: 'Connect' }).first().click();
 };
